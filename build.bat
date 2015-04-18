007b3d66bcf739a36d40add17b0e66b40d7fe15f
James <james@jadaradix.com>
2015-04-18 17:06:33 +0100

    Build, and git shit.

---
+++ build.bat
@@ -0,0 +1,3 @@
+copy ODFXMaker\bin\Debug\ODFXMaker.exe DarkFlow\bin\Debug\ODFXMaker.exe
+copy HTML5\bin\Debug\HTML5.exe DarkFlow\bin\Debug\PlatformProfiles\HTML5.exe
+copy PCRunner\bin\Debug\PCRunner.exe DarkFlow\bin\Debug\PlatformProfiles\Windows.exe
